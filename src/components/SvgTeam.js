ed6868518646f745551b0f5f9308541935d7e611
twismdx <twism@live.com>
2023-12-17 15:51:37 +1030

    a

--- src/components/SvgTeam.js
+++ src/components/SvgTeam.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import ProgressBar from "@ramonak/react-progress-bar"
 import '../home.css'
 import { useGlobalContext } from './Context'
 
@@ -9,51 +10,232 @@ const SvgTeam = () => {
 	const [useExt, setUseExt] = useState(false)
 
 	return (
-		<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="80%" viewBox="0 0 1629.26 117">
-			<defs>
-				<linearGradient id="linear-gradient" x1="729.65" y1="1372.1" x2="922.01" y2="1383.85" gradientTransform="translate(0 -1348)" gradientUnits="userSpaceOnUse">
-					<stop offset="0" stop-color="#f7f5f5" />
-					<stop offset="0.54" stop-color="#d9d8d8" />
-				</linearGradient>
-			</defs>
-			<g id="Group_1" data-name="Group 1">
-				<path id="Frame" d="M30,7.7A15,15,0,0,1,43.11,0h1582.5a15,15,0,0,1,12.8,22.8l-18.3,30a14.93,14.93,0,0,1-12.8,7.2H26.41a15,15,0,0,1-13.1-22.3Z" transform="translate(-11.37)" style={{ fillOpacity: "0.5" }} />
-				<path id="HomeFrame" d="M30.91,9.4a15.11,15.11,0,0,1,13-7.4h674.4V58H28.51a15,15,0,0,1-12.9-22.6Z" transform="translate(-11.37)" style={{ fill: "#1c1c1c" }} />
-				<path id="AwayFrame" d="M718.31,2h904.9a15,15,0,0,1,12.7,23l-16.2,26a14.93,14.93,0,0,1-12.7,7H718.31Z" transform="translate(-11.37)" style={{ fill: "#1c1c1c" }} />
-				<path id="Center" d="M733.31,5h186a15,15,0,0,1,15,15V40a15,15,0,0,1-15,15h-186a15,15,0,0,1-15-15V20A15,15,0,0,1,733.31,5Z" transform="translate(-11.37)" style={{ fill: "url(#linear-gradient)" }} />
-				<rect id="ProgressBarFrame" x="391.94" y="71" width="218" height="46" style={{ fill: "#fff" }} />
-				<rect id="ProgressBar" x="393.94" y="73" width="214" height="42" style={{ fill: "#fff" }} />
-				<path id="Extension" d="M343.31,86a15,15,0,0,1,15-15h45v46h-45a15,15,0,0,1-15-15Z" transform="translate(-11.37)" style={{ fill: useExt ? "#d91a1a" : "#1c1c1c" }} />
-				<path id="Time" d="M621.31,71h50.8c14.5,0,26.2,.7,26.2,15v16c0,8.3-11.7,15-26.2,15H621.31Z" transform="translate(-11.37)" style={{ fill: "#1c1c1c" }} />
-				<path id="Gloss" d="M343.31,92c0-11.6,11-21,24.5-21h376c9.7,0,17.5,6.7,17.5,15v6Z" transform="translate(-11.37)" style={{ fill: "#d9d9d9", fillOpacity: "0.07999999821186066" }} />
-				{useExt ?
-					<g style={{ isolation: "isolate" }}>
-						<path d="M348.11,103V84h13.6v4.1h-8.5v3.2H361v4.1h-7.8v3.2h8.5v4.1h-13.6Z" transform="translate(-11.37)" style={{ fill: "#fff" }} />
-						<path d="M369.81,84l3.1,5.5h.1l3.1-5.5h5.7l-5.7,9.5L382,103h-5.9l-3.2-5.6h-.1l-3.2,5.6h-5.8l5.8-9.5L364,84Z" transform="translate(-11.37)" style={{ fill: "#fff" }} />
-						<path d="M383.61,88.2V84H400v4.1h-5.7V103h-5V88.2Z" transform="translate(-11.37)" style={{ fill: "#fff" }} />
+		<>
+			<div>
+				<ProgressBar completed={1} height="46" width="218" borderRadius="20px" isLabelVisible={false} />
+			</div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				xmlnsXlink="http://www.w3.org/1999/xlink"
+				data-name="Layer 1"
+				width="80%"
+				viewBox="0 0 1629.26 117"
+			>
+				<defs>
+					<linearGradient
+						id="a"
+						x1={847.01}
+						x2={998.65}
+						y1={-1734.35}
+						y2={-1743.61}
+						gradientTransform="matrix(1 0 0 -1 0 -1904)"
+						gradientUnits="userSpaceOnUse"
+					>
+						<stop offset={0} stopColor="#f7f5f5" />
+						<stop offset={0.54} stopColor="#d9d8d8" />
+					</linearGradient>
+					<linearGradient
+						xlinkHref="#a"
+						id="b"
+						x1={1042.47}
+						x2={1132.19}
+						y1={-1736.25}
+						y2={-1741.73}
+					/>
+					<linearGradient
+						xlinkHref="#a"
+						id="c"
+						x1={713.17}
+						x2={802.89}
+						y1={-1736.25}
+						y2={-1741.73}
+					/>
+				</defs>
+				<g data-name="Group 1">
+					<path
+						d="M126.9-187.3A15 15 0 0 1 140-195h1582.5a15 15 0 0 1 12.8 22.8l-18.3 30a14.93 14.93 0 0 1-12.8 7.2H123.3a15 15 0 0 1-13.1-22.3Z"
+						style={{
+							fillOpacity: 0.5,
+						}}
+						transform="translate(-108.26 195)"
+					/>
+					<path
+						d="M127.8-185.6a15.11 15.11 0 0 1 13-7.4h674.4v56H125.4a15 15 0 0 1-12.9-22.6ZM815.2-193h904.9a15 15 0 0 1 12.7 23l-16.2 26a14.93 14.93 0 0 1-12.7 7H815.2Z"
+						style={{
+							fill: "#1c1c1c",
+						}}
+						transform="translate(-108.26 195)"
+					/>
+					<path
+						d="M850-190h146.4c6.5 0 11.8 6.7 11.8 15v20c0 8.3-5.3 15-11.8 15H850c-6.5 0-11.8-6.7-11.8-15v-20c0-8.3 5.3-15 11.8-15Z"
+						style={{
+							fill: "url(#a)",
+						}}
+						transform="translate(-108.26 195)"
+					/>
+					<path
+						d="M391.94 71h218v46h-218z"
+						style={{
+							fill: "#fff",
+						}}
+					/>
+					<path
+						d="M393.94 73h214v42h-214z"
+						data-name="progress bar"
+						style={{
+							fill: "#fff",
+						}}
+					/>
+					<path
+						d="M440.2-109a15 15 0 0 1 15-15h45v46h-45a15 15 0 0 1-15-15Z"
+						style={{
+							fill: "#c45353",
+						}}
+						transform="translate(-108.26 195)"
+					/>
+					<path
+						d="M718.2-124H832c14.5 0 26.2 6.7 26.2 15v16c0 8.3-11.7 15-26.2 15H718.2Z"
+						style={{
+							fill: "#1c1c1c",
+						}}
+						transform="translate(-108.26 195)"
+					/>
+					<path
+						d="M440.2-103c0-11.6 11-21 24.5-21h376c9.7 0 17.5 6.7 17.5 15v6Z"
+						style={{
+							fill: "#d9d9d9",
+							fillOpacity: 0.07999999821186066,
+						}}
+						transform="translate(-108.26 195)"
+					/>
+					<g
+						style={{
+							isolation: "isolate",
+						}}
+					>
+						<path
+							d="M445-92v-19h13.6v4.1h-8.5v3.2h7.8v4.1h-7.8v3.2h8.5v4.1H445ZM466.7-111l3.1 5.5h.1l3.1-5.5h5.7l-5.7 9.5 5.9 9.5H473l-3.2-5.6h-.1l-3.2 5.6h-5.8l5.8-9.5-5.6-9.5ZM480.5-106.8v-4.2h16.4v4.1h-5.7V-92h-5v-14.8Z"
+							style={{
+								fill: "#fff",
+							}}
+							transform="translate(-108.26 195)"
+						/>
 					</g>
-					: ''
-				}
-			</g>
-			<rect id="homeplayer" x="81.54" y="7.5" width="500.8" height="45.1" style={{ fill: "none" }} />
-			<rect id="awayplayer" x="1046.94" y="7.5" width="500.8" height="45.1" style={{ fill: "none" }} />
-			<rect id="homescore" x="599.54" y="7.5" width="100.9" height="45.1" style={{ fill: "none" }} />
-			<rect id="awayscore" x="928.84" y="7.5" width="100.9" height="45.1" style={{ fill: "none" }} />
-			<rect id="raceto" x="725.34" y="7.5" width="175.6" height="45.1" style={{ fill: "none" }} />
-			<rect id="timer" x="614.94" y="73" width="98" height="42" style={{ fill: "none" }} />
-			<text id="timer-2" textAnchor='middle' data-name="timer" transform="translate(663.94 101.4)" style={{ isolation: "isolate", fontSize: "40px", fill: "#fff", fontFamily: "semibold" }}>
-			</text>
-			<text id="p1name" textAnchor='middle' transform="translate(323.47 42.5)" style={{ isolation: "isolate", fontSize: "40px", fill: "#fff", fontFamily: "semibold" }}>
-				{stats[0] ? stats[0].hometeamlabel : ''}</text>
-			<text id="p2name" textAnchor='middle' transform="translate(1289.85 42.5)" style={{ isolation: "isolate", fontSize: "40px", fill: "#fff", fontFamily: "semibold" }}>
-				{stats[0] ? stats[0].awayteamlabel : ''}</text>
-			<text id="p2score" textAnchor='middle' transform="translate(971.77 45.5)" style={{ isolation: "isolate", fontSize: "50px", fill: "#fff", fontFamily: "semibold" }}>
-				{stats[0] ? stats[0].awayscore : ''}</text>
-			<text id="p1score" textAnchor='middle' transform="translate(642 45.5)" style={{ isolation: "isolate", fontSize: "50px", fill: "#fff", fontFamily: "semibold" }}>
-				{stats[0] ? stats[0].homescore : ''}</text>
-			<text id="raceto-2" textAnchor='middle' data-name="raceto" transform="translate(812.43 41)" style={{ isolation: "isolate", textAlign: "center", textAnchor: "center", fontSize: "30px", fill: "#231f20", fontFamily: "semibold" }}>
-				Race to</text >
-		</svg>
+					<path
+						d="M1044.5-190h86.1c3.8 0 6.9 6.7 6.9 15v20c0 8.3-3.1 15-6.9 15h-86.1c-3.8 0-6.9-6.7-6.9-15v-20c0-8.3 3.1-15 6.9-15Z"
+						data-name="Center"
+						style={{
+							fill: "url(#b)",
+						}}
+						transform="translate(-108.26 195)"
+					/>
+					<path
+						d="M715.2-190h86.1c3.8 0 6.9 6.7 6.9 15v20c0 8.3-3.1 15-6.9 15h-86.1c-3.8 0-6.9-6.7-6.9-15v-20c-.1-8.3 3.1-15 6.9-15Z"
+						data-name="Center"
+						style={{
+							fill: "url(#c)",
+						}}
+						transform="translate(-108.26 195)"
+					/>
+				</g>
+				<path
+					d="M81.54 7.5h500.8v45.1H81.54zM1046.94 7.5h500.8v45.1h-500.8zM599.54 7.5h100.9v45.1h-100.9zM928.84 7.5h100.9v45.1h-100.9zM725.34 7.5h175.6v45.1h-175.6zM614.94 73h98v42h-98z"
+					style={{
+						fill: "none",
+					}}
+				/>
+				<text
+					data-name="timer"
+					style={{
+						isolation: "isolate",
+						fontSize: 30,
+						fill: "#fff",
+						fontFamily: "xxBold",
+						fontVariant: "small-caps",
+						fontWeight: "900",
+						textAlign: "center",
+						textAnchor: "middle",
+					}}
+					transform="translate(675 105)"
+				>
+					00:59
+				</text>
+				<text
+					style={{
+						isolation: "isolate",
+						fontSize: 40,
+						fill: "#fff",
+						fontFamily: "xxBold",
+						fontVariant: "small-caps",
+						fontWeight: "900",
+						textAlign: "center",
+						textAnchor: "middle",
+					}}
+					transform="translate(323.48 40)"
+				>
+					{stats[0] ? stats[0].hometeamlabel : 'damien'}
+				</text>
+				<text
+					style={{
+						isolation: "isolate",
+						fontSize: 40,
+						fill: "#fff",
+						fontFamily: "xxBold",
+						fontVariant: "small-caps",
+						fontWeight: "900",
+						textAlign: "center",
+						textAnchor: "middle",
+					}}
+					transform="translate(1289.85 40)"
+				>
+					{stats[0] ? stats[0].awayteamlabel : 'damien'}
+				</text>
+				<text
+					style={{
+						isolation: "isolate",
+						fontSize: 40,
+						fontFamily: "xxBold",
+						fontVariant: "small-caps",
+						fontWeight: "900",
+						textAlign: "center",
+						textAnchor: "middle",
+					}}
+					transform="translate(977.5 44)"
+				>
+					{stats[0] ? stats[0].awayscore : '11'}
+				</text>
+				<text
+					style={{
+						isolation: "isolate",
+						fontSize: 40,
+						fontFamily: "xxBold",
+						fontVariant: "small-caps",
+						fontWeight: "900",
+						textAlign: "center",
+						textAnchor: "middle",
+					}}
+					transform="translate(648 44)"
+				>
+					{stats[0] ? stats[0].homescore : '11'}
+				</text>
+				<text
+					data-name="raceto"
+					style={{
+						isolation: "isolate",
+						fontSize: 30,
+						fill: "#231f20",
+						fontFamily: "xxBold",
+						fontVariant: "small-caps",
+						fontWeight: "900",
+						textAlign: "center",
+						textAnchor: "middle",
+					}}
+					transform="translate(815 40)"
+				>
+					Race to 15
+				</text>
+			</svg>
+		</>
 	)
 }
 // {stats[0].awayframepointsadj===0 && stats[0].awayscorepoints===0 ? stats[0].awayscore : `${awayScore}`}
