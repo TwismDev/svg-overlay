29bbf839514effb859bec2f14973461b90f62b26
twismdx <twism@live.com>
2024-01-01 17:28:33 +1030

    a

--- api/serverless-function.js
+++ api/serverless-function.js
@@ -1,11 +1,9 @@
 import { useGlobalContext } from '../src/components/Context'
 
 const { setId } = useGlobalContext()
-module.exports = (req, res) => {
-    const searchParams = new URLSearchParams(req.url.split('?')[1])
-    const id = searchParams.get('id')
 
+export default async (req, res) => {
+    const { id } = req.query
     setId(id)
-
-    res.status(200).send('Received id: ' + id)
+    res.status.send(resonse.text)
 }
