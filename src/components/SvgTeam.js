fd73c58df1314c5567698e1f565256f26ef73810
twismdx <twism@live.com>
2024-01-23 08:01:20 +1030

    a

--- src/components/SvgTeam.js
+++ src/components/SvgTeam.js
@@ -22,7 +22,9 @@ const SvgTeam = ({ stats }) => {
 				xmlns='http://www.w3.org/2000/svg'
 				xmlnsXlink='http://www.w3.org/1999/xlink'
 				data-name='Layer 1'
-				viewBox='0 0 1604.39 179.64'
+				// viewBox='0 0 1604.39 179.64'
+				width='100%'
+				height='100%'
 				style={{ transform: 'scaleY(0.8)', transform: 'scale(5)' }}
 			>
 				<defs>
